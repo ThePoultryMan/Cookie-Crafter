ef81613f4db6d258f85e077705a128811c39c50f
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-08-27 15:13:54 -0400

    refactor: better achievement loader

--- common/src/main/java/io/github/thepoultryman/cookiecrafter/achievement/AchievementLoader.java
+++ common/src/main/java/io/github/thepoultryman/cookiecrafter/achievement/AchievementLoader.java
@@ -4,17 +4,30 @@ import com.google.gson.Gson;
 import io.github.thepoultryman.cookiecrafter.Cookiecrafter;
 import io.github.thepoultryman.cookiecrafter.ResourceHelper;
 
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class AchievementLoader {
+    public static HashMap<String, Achievement> achievementMap;
+
     public static void loadAchievements() {
         Gson gson = new Gson();
         AtomicInteger count = new AtomicInteger();
+        AtomicInteger totalAchievements = new AtomicInteger();
 
-        ResourceHelper.loadResource("achievements/wake_and_bake.json").ifPresent((reader) -> {
-            Achievement achievement = gson.fromJson(reader, Achievement.class);
-            count.getAndIncrement();
+        ResourceHelper.loadResource("achievements/_list.json").ifPresent((reader) -> {
+            String[] achievements = gson.fromJson(reader, String[].class);
+            totalAchievements.set(achievements.length);
+            achievementMap = new HashMap<>(achievements.length);
+
+            for (String achievement : achievements) {
+                ResourceHelper.loadResource("achievements/" + achievement + ".json").ifPresent((achievementReader) -> {
+                    achievementMap.put(achievement, gson.fromJson(achievementReader, Achievement.class));
+                    count.getAndIncrement();
+                });
+            }
         });
-        Cookiecrafter.LOGGER.info("Loaded " + count + " achievements.");
+
+        Cookiecrafter.LOGGER.info("Loaded " + count + "/" + totalAchievements + " achievements.");
     }
 }
